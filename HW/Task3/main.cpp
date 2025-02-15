37dcd2951651fe182556ab6802b0eec1e0790032
OG_Michael <59878471+M1SH4-cmd@users.noreply.github.com>
2025-02-15 07:06:10 +0700

    Task3 reworked

--- HW/Task3/main.cpp
+++ HW/Task3/main.cpp
@@ -3,64 +3,82 @@
 #include <mutex>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
 
-std::mutex kitchenMutex;
-bool isKitchenFree = true;
-int completedDeliveries = 0;
+std::mutex orderMutex;
+std::mutex readyMutex;
+std::vector<std::string> order_queue;
+std::vector<std::string> ready_orders;
+int delivered = 0;
 
 std::string getRandomDish() {
     const std::vector<std::string> dishes = {"Pizza", "Soup", "Steak", "Salad", "Sushi"};
     return dishes[rand() % dishes.size()];
 }
 
-void kitchen() {
-    while (completedDeliveries < 10) {
+void orderThread() {
+    while (delivered < 10) {
         std::this_thread::sleep_for(std::chrono::seconds(5 + rand() % 6));
         std::string dish = getRandomDish();
 
-        kitchenMutex.lock();
-        while (!isKitchenFree) {
-            kitchenMutex.unlock();
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            kitchenMutex.lock();
-        }
-
-        isKitchenFree = false;
-        kitchenMutex.unlock();
-
-        std::cout << "Kitchen is preparing " << dish << "..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(5 + rand() % 11));
-
-        kitchenMutex.lock();
-        isKitchenFree = true;
-        std::cout << dish << " is ready!" << std::endl;
-        kitchenMutex.unlock();
+        orderMutex.lock();
+        order_queue.push_back(dish);
+        std::cout << "New order: " << dish << std::endl;
+        orderMutex.unlock();
     }
 }
 
-void courier() {
-    while (completedDeliveries < 10) {
-        std::this_thread::sleep_for(std::chrono::seconds(30));
-        std::cout << "Courier is picking up the orders..." << std::endl;
+void kitchenThread() {
+    while (delivered < 10) {
+        orderMutex.lock();
+        if (!order_queue.empty()) {
+            std::string dish = order_queue.front();
+            order_queue.erase(order_queue.begin());
+            orderMutex.unlock();
 
-        kitchenMutex.lock();
-        if (isKitchenFree) {
-            completedDeliveries++;
-            std::cout << "Courier has delivered an order. Total deliveries: " << completedDeliveries << std::endl;
+            std::cout << "Cooking: " << dish << "..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(5 + rand() % 11));
+
+            readyMutex.lock();
+            ready_orders.push_back(dish);
+            std::cout << dish << " is ready!" << std::endl;
+            readyMutex.unlock();
+        } else {
+            orderMutex.unlock();
         }
-        kitchenMutex.unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Небольшая задержка для проверки очереди
+    }
+}
+
+// Поток для доставки
+void courierThread() {
+    while (delivered < 10) {
+        std::this_thread::sleep_for(std::chrono::seconds(30));
+
+        readyMutex.lock();
+        if (!ready_orders.empty()) {
+            std::string dish = ready_orders.front();
+            ready_orders.erase(ready_orders.begin());
+            delivered++;
+            std::cout << "Courier delivered: " << dish << ". Total deliveries: " << delivered << std::endl;
+        }
+        readyMutex.unlock();
     }
 }
 
 int main() {
     srand(time(0));
 
-    std::thread kitchenThread(kitchen);
-    std::thread courierThread(courier);
+    std::thread order(orderThread);
+    std::thread kitchen(kitchenThread);
+    std::thread courier(courierThread);
 
-    kitchenThread.detach();
-    courierThread.detach();
+    order.join();
+    kitchen.join();
+    courier.join();
 
-    std::this_thread::sleep_for(std::chrono::seconds(300)); // Дал программе время на выполнение
+    std::cout << "Program finished after 10 deliveries." << std::endl;
     return 0;
 }
